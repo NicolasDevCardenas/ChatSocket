7aa64fd0978b3568a1716e227ecc6bf2bcb600ed
Nicolas Cardenas <ncardenas@grupodot.com>
2019-01-30 11:02:09 -0500

    Chat Implementado

--- client/main.js
+++ client/main.js
@@ -1,1 +1,30 @@
-var socket = io.connect('http://172.28.4.198:6677',{'forceNew':true});
+var socket = io.connect('http://172.28.9.39:6677',{'forceNew':true});
+
+socket.on("messages", function (data) {
+    console.log(data);
+    render(data);
+});
+
+function render(data) {
+    var html = data.map(function (message, index) {
+        return(
+            `
+            <div class="message">
+                <strong>${message.nickname}</strong> dice:
+                <p>${message.text}</p>
+            </div>
+            `
+        );
+    }).join('');
+
+    document.getElementById('message').innerHTML = html;
+}
+function addMessage(e) {
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message);
+    return false;
+}
